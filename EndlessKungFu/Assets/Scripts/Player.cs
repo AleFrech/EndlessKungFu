6880586cf8b86ae019677252dbf5348e355eee44
Alejandro-Frech <alefrech@gmail.com>
2015-10-25 20:14:50 -0600

    punch right,crouch

--- EndlessKungFu/Assets/Scripts/Player.cs
+++ EndlessKungFu/Assets/Scripts/Player.cs
@@ -3,10 +3,12 @@ using System.Collections;
 
 public class Player : MonoBehaviour {
 	
-	public float speed = 3f;
+	public float speed = 1f;
 	public bool lookingLeft = true;
 	public bool walking = false;
 	public bool attacking = false;
+    public bool crouching = false;
+    public bool kicking = false;
 	private Rigidbody2D rb2d;
 	private Animator anim;
 	// Use this for initialization
@@ -18,28 +20,42 @@ public class Player : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKey (KeyCode.RightArrow) && lookingLeft) {
+        crouching = (Input.GetKey(KeyCode.DownArrow));
+        print((Input.GetKey(KeyCode.DownArrow)));
+        if (Input.GetKey (KeyCode.RightArrow) && lookingLeft) {
 			lookingLeft = false;
 		} else if (Input.GetKey (KeyCode.LeftArrow) && !lookingLeft) {
 			lookingLeft = true;
 		}
-
-		if ((Input.GetKey (KeyCode.LeftArrow) && lookingLeft) || (Input.GetKey (KeyCode.RightArrow) && !lookingLeft)) {
+        
+		if (((Input.GetKey (KeyCode.LeftArrow) && lookingLeft) || (Input.GetKey (KeyCode.RightArrow) && !lookingLeft))&&!crouching) {
 			walking = true;
 		} else{ walking = false;}
 
-		attacking = (Input.GetKey (KeyCode.S));
+        //if ((lookingLeft && Input.GetKeyUp(KeyCode.LeftArrow)) || (!lookingLeft && Input.GetKeyUp(KeyCode.RightArrow)))
+        //{
+        //    walking = false;
+        //}
 
-		anim.SetBool ("Attacking", attacking);
+        attacking = (Input.GetKey (KeyCode.S));
+        kicking = (Input.GetKey(KeyCode.A));
 
-		//Debug.Log (walking.ToString());
-		anim.SetBool("LookingLeft", lookingLeft);
+        anim.SetBool ("Attacking", attacking);
+        anim.SetBool("Crouching",crouching);
+        anim.SetBool("Kicking", kicking);
+
+
+        //Debug.Log (walking.ToString());
+        anim.SetBool("LookingLeft", lookingLeft);
 		anim.SetBool ("Walking", walking);
 		//rb2d.AddForce(Vector2.right*move*speed);
 	}
 
 	void FixedUpdate(){
-		float move = Input.GetAxis("Horizontal");
-		rb2d.velocity = new Vector2 (move * speed, rb2d.velocity.y);
+	    if (walking&&!attacking)
+	    {
+	        float move = Input.GetAxis("Horizontal")/2;
+	        rb2d.velocity = new Vector2(move*speed, rb2d.velocity.y);
+	    }
 	}
 }
